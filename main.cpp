a2431e0a449e93e2f1fc31fc86d76fc2a7c14be1
Kotori Saki <kitartist1th@gmail.com>
2025-01-20 22:31:14 +0700

    Added UI about Game's name as a function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
+void UI_name(short clear){
+    if(clear == 1){system("cls");}
+    cout << "\n\t\t\\__________________/ \n\t\t/                  \\ \n\t\t|  @ gonna hunt A  |\n\t\t\\__________________/ \n\t\t/                  \\ \n\n\n";
+}
+
 int main(){
-    cout << "Hello world";
+    UI_name(1);
+    //----------------------
 }
