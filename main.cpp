f63405c6a1f306536ffa8862fea7a88009bf439d
Kotori Saki <kitartist1th@gmail.com>
2025-01-21 17:38:35 +0700

    Added FUNC_delay, startMenu Improve UI Rendered at main() with FUNC_delay

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@
 
 using namespace std;
 
+void FUNC_delay(int mili){this_thread::sleep_for(chrono::milliseconds(mili));}
+
 void UI_hello(){
+    system("cls");//!clear
     cout << ("H   H    EEEEE   L       L        OOOOO\n");
     cout << ("H   H    E       L       L       O     O\n");
     cout << ("H   H    E       L       L       O     O\n");
@@ -45,13 +48,36 @@ void UI_loadingBar(){
     this_thread::sleep_for(chrono::milliseconds(200));
 }
 
+//------------------
+void startMenu();//*rendered start menu and require the chosen
+//------------------
+
 int main(){
     UI_hello();
-    this_thread::sleep_for(chrono::milliseconds(1000));
+    FUNC_delay(1000);
     UI_name(1);
-    this_thread::sleep_for(chrono::milliseconds(200));
+    FUNC_delay(200);
     UI_loadingBar();
     UI_cover("upper");
     cout << "\tPress Enter..\n\n\n"; getchar();
     //----------------------
+    startMenu();
+}
+
+void startMenu(){
+    string choice1;
+    int checker_1st_time = 0;
+    while(1){
+        system("cls");//!clear
+        UI_cover("upper");
+        if(checker_1st_time != 1){FUNC_delay(200);}
+        cout << "\t1] Guider gameplay" << endl
+             << "\t2] Start match" << endl
+             << "\t3] Credits" << endl << endl;
+        FUNC_delay(200);
+        if(checker_1st_time == 1){checker_1st_time = 0; cout << "\t\t! [Only 1 - 3]" << endl;}
+        cout << "\t\t=] "; getline(cin,choice1);
+        if(choice1.compare("1") == 0 || choice1.compare("2") == 0 || choice1.compare("3") == 0){break;}
+        checker_1st_time = 1;
+    }
 }
